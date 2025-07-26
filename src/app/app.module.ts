de6a7f6cab6882edb9ef0ba3d03856255040345f
yagof <yagofer12@gmail.com>
2025-07-26 18:28:03 -0300

    modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [BrowserModule, RouterModule],
+  providers: [],
+  bootstrap: [],
 })
 export class AppModule {}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ComponentsModule } from './components/components.module';
 import { LoginComponent } from './login/login.component';
+import { PagesRoutingModule } from './pages-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, ComponentsModule],
+  imports: [CommonModule, ComponentsModule, PagesRoutingModule, SharedModule],
 })
 export class PagesModule {}
