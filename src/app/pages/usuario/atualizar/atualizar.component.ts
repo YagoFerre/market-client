39851588639d5ba2ca08e559725b5d4dfc2d8c6d
yagof <yagofer12@gmail.com>
2025-07-26 01:01:36 -0300

    paginas estrutura base

---
+++ src/app/pages/usuario/atualizar/atualizar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-atualizar',
+  imports: [],
+  templateUrl: './atualizar.component.html',
+  styleUrl: './atualizar.component.css'
+})
+export class AtualizarComponent {
+
+}
