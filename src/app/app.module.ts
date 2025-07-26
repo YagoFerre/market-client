f97b778f631556a9e2bfaa244d3873d65fda7c9c
yagof <yagofer12@gmail.com>
2025-07-26 18:11:55 -0300

    save changes

---
+++ src/app/app.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [CommonModule, SharedModule],
+})
+export class AppModule {}
---
+++ src/app/pages/produto/detalhes/detalhes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhes',
+  imports: [],
+  templateUrl: './detalhes.component.html',
+  styleUrl: './detalhes.component.css'
+})
+export class DetalhesComponent {
+
+}
